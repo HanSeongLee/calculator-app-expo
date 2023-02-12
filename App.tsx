13961223f10d872b4821c257f4f97c1e9e0bb2dd
HanSeongLee <jkhanseong@gmail.com>
2023-02-12 16:58:27 +0900

    ADD | Custom fonts

--- App.tsx
+++ App.tsx
@@ -1,7 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Font from 'expo-font';
+import { useEffect, useState } from 'react';
 
 export default function App() {
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+
+  const loadCustomFonts = async () => {
+    await Font.loadAsync({
+      'League Spartan': require('./assets/fonts/LeagueSpartan-Bold.ttf'),
+    });
+    setFontsLoaded(true);
+  };
+
+  useEffect(() => {
+    loadCustomFonts();
+  }, []);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
